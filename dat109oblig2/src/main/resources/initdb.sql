e87efb87951196f9dda7f027f3a7e1c540b163c4
leo <heldal.leonard@gmail.com>
2024-02-07 19:53:19 +0100

    idk

--- dat109oblig2/src/main/resources/initdb.sql
+++ dat109oblig2/src/main/resources/initdb.sql
@@ -3,14 +3,41 @@ create schema dat109_oblig2;
 
 set search_path to dat109_oblig2;
 
-
-
-create table car(
-                    registrationNr varchar(8) primary key,
-                    brand varchar(30),
-                    model varchar(30),
-                    color varchar(30),
-                    groupCar varchar(30),
-                    availibe boolean
+create table address(
 
 );
+
+create table costumer(
+
+);
+
+create table rentalCompany(
+
+);
+
+create table rentalOffice(
+
+);
+
+create table rentalOrder(
+
+);
+
+create table car(
+    registrationNr varchar(8) primary key,
+    brand varchar(30),
+    model varchar(30),
+    color varchar(30),
+    groupCar varchar(30),
+    availibe bool
+
+);
+
+
+
+create table rentalOrder(
+
+);
+
+
+
