f34d8059db831ec879c94a59a4aae009c0febf85
leo <heldal.leonard@gmail.com>
2024-02-08 01:09:04 +0100

    refractor, unit test skeleton, generic dao

---
+++ dat109oblig2/target/classes/initdb.sql
@@ -0,0 +1,37 @@
+drop schema if exists dat109_oblig2 cascade;
+create schema dat109_oblig2;
+
+set search_path to dat109_oblig2;
+
+create table address(
+
+);
+
+create table costumer(
+
+);
+
+create table rentalCompany(
+
+);
+
+create table rentalOffice(
+
+);
+
+create table rentalOrder(
+
+);
+
+create table car(
+    registrationNr varchar(8) primary key,
+    brand varchar(30),
+    model varchar(30),
+    color varchar(30),
+    groupCar varchar(30),
+    available boolean
+
+);
+
+
+
